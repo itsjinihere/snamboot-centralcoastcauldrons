8dc1014e86e7476ca6daacc08b3b4c167081cc37
Sreerenjini Surendran Namboothiri <snamboot@calpoly.edu>
2025-04-08 22:40:55 -0700

    Fix job errors, pass all tests and linting

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Depends, status
 from pydantic import BaseModel, Field, field_validator
-from typing import List
 from src.api import auth
 import sqlalchemy
 from src import database as db
